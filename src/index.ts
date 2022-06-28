75a8ec515f89c8c40eb596f09b88944d841a95af
wunlei <confusioncosmos@gmail.com>
2022-06-29 00:18:41 +0300

    refactor: update modules imports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { server } from "./server/server";
+import { DEFAULT_PORT } from "./server/server.constants";
 
-server.listen({ port: process.env.PORT }).then(({ url }) => {
+server.listen({ port: process.env.PORT || DEFAULT_PORT }).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,16 +1,19 @@
 import { ApolloServer } from "apollo-server";
-import typeDefsAlbums from "../modules/albums/albums.schema";
-import AlbumsService from "../modules/albums/albums.services";
-import typeDefsArtist from "../modules/artists/artist.schema";
-import ArtistService from "../modules/artists/artist.services";
-import typeDefsBands from "../modules/bands/bands.schema";
-import BandsService from "../modules/bands/bands.services";
-import typeDefsGenres from "../modules/genres/genres.schema";
-import GenresService from "../modules/genres/genres.services";
-import typeDefsTracks from "../modules/tracks/tracks.schema";
-import TracksService from "../modules/tracks/tracks.services";
-import typeDefsUser from "../modules/users/user.schema";
-import UsersService from "../modules/users/users.services";
+import {
+  AlbumsService,
+  ArtistService,
+  BandsService,
+  GenresService,
+  TracksService,
+  typeDefsAlbums,
+  typeDefsArtist,
+  typeDefsBands,
+  typeDefsGenres,
+  typeDefsTracks,
+  typeDefsUser,
+  UsersService,
+} from "../modules/index";
+
 import typeDefsServer from "./server.schema";
 
 export const dataSources = {
